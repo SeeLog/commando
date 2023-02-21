91ac632db0ba51e526e517c9b58673a418436cea
SeeLog <seelog693@gmail.com>
2023-02-22 01:32:00 +0900

    [update] move unittest file

--- src/test/unittest/interface.test.ts
+++ src/test/unittest/interface.test.ts
@@ -1,8 +1,8 @@
 import * as assert from 'assert';
 import { defaultConfig } from '../../interface';
 
-suite('Interface Test Suite', () => {
-  test('Make sure defaultConfig is IConfig', () => {
+describe('Interface Test', () => {
+  it('should satisfy IConfig structure', () => {
     assert.strictEqual(typeof defaultConfig.autoClear, 'boolean');
     assert.strictEqual(typeof defaultConfig.runOnTerminal, 'boolean');
     assert.strictEqual(Array.isArray(defaultConfig.commands), true);
