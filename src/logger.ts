219ef06daa0f168885420ee9bbb1874c251bd9ca
SeeLog <seelog693@gmail.com>
2023-02-27 01:08:39 +0900

    Run on terminal (#6)
    * [add] support run in terminal
    
    * [add] test
    
    * [update] allow to run in existing terminal

--- src/logger.ts
+++ src/logger.ts
@@ -5,5 +5,11 @@ export const getOutputChannel = (name: string): vscode.OutputChannel => {
 };
 
 export const getTerminal = (name: string): vscode.Terminal => {
+  // Check if terminal already exists
+  const terminal = vscode.window.terminals.find((terminal) => terminal.name === name);
+  if (terminal) {
+    return terminal;
+  }
+  // Create new terminal
   return vscode.window.createTerminal(name);
 };
