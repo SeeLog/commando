d9d4543df836f66794b1fde6601f429b74fbefb4
SeeLog <seelog693@gmail.com>
2023-02-26 23:24:03 +0900

    [add] convert on windows

--- src/command.ts
+++ src/command.ts
@@ -1,8 +1,10 @@
 import * as vscode from 'vscode';
+import { platform } from 'os';
+import * as iconv from 'iconv-lite';
 import { ICommand, IConfig, formatReplaceKeys, FormatReplaceKey, defaultConfig } from './interface';
 import { fmt } from './util';
 import { getOutputChannel, getTerminal } from './logger';
-import { exec } from 'child_process';
+import { exec, ExecOptions } from 'child_process';
 
 /*
  * Run command in VSCode output channel or terminal
@@ -24,14 +26,15 @@ export const runCommandInOutputChannel = async (command: ICommand, config: IConf
   if (command.autoClear || (config.autoClear && command.autoClear !== false)) {
     outputChannel.clear();
   }
+  const options = getExecOptions(config);
 
   // exec command
-  const child = exec(command.cmd);
+  const child = exec(command.cmd, options);
   child.stdout?.on('data', (data) => {
-    outputChannel.append(data);
+    outputChannel.append(convertExecOutput(data));
   });
   child.stderr?.on('data', (data) => {
-    outputChannel.append(data);
+    outputChannel.append(convertExecOutput(data));
   });
   child.on('error', (error) => {
     outputChannel.append(error.message);
@@ -58,4 +61,35 @@ export const getWindowName = (command: ICommand, config: IConfig): string => {
   return fmt(config.windowName ?? defaultConfig.windowName, {
     [formatReplaceKeys.commandName]: command.name,
   });
+};
+
+const getExecOptions = (config: IConfig): ExecOptions => {
+  const dir = getWorkingDir(vscode.window.activeTextEditor?.document.uri);
+  const execOptions: ExecOptions = {
+    shell: config.shell,
+    cwd: dir,
+  };
+  return execOptions;
+};
+
+/**
+ * Get working directory.
+ * If the file is in a workspace folder, use that folder.
+ * Otherwise, use the first workspace folder.
+ * @param uri file uri
+ * @returns working directory
+ */
+const getWorkingDir = (uri: vscode.Uri | undefined): string | undefined => {
+  if (!uri) {
+    return undefined;
+  }
+  const fileWorkspaceFolder = vscode.workspace.getWorkspaceFolder(uri);
+  return fileWorkspaceFolder ? fileWorkspaceFolder.uri.fsPath : vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+};
+
+const convertExecOutput = (output: string): string => {
+  if (platform() === 'win32') {
+    return iconv.decode(Buffer.from(output), 'cp936');
+  }
+  return output;
 };
