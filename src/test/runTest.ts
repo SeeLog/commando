beb4ff6bbdd4c280637645d6dd68569b7baa614e
SeeLog <seelog693@gmail.com>
2023-02-27 04:21:15 +0900

    [add] test setting

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -8,12 +8,14 @@ async function main() {
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
+    const workspacePath = path.resolve(extensionDevelopmentPath, 'src/test/workspace');
+
     // The path to test runner
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [workspacePath] });
   } catch (err) {
     console.error('Failed to run tests', err);
     process.exit(1);
