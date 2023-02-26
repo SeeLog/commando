5ccdd66ec44e722468edd0afb0b62419811a61eb
SeeLog <seelog693@gmail.com>
2023-02-27 01:04:38 +0900

    [add] test

--- src/test/suite/command.test.ts
+++ src/test/suite/command.test.ts
@@ -45,14 +45,46 @@ suite('Command test', function () {
         replace: () => {
           // do nothing
         },
-        getResult: () => {
-          return outputResult;
-        },
       };
     };
     return {
       getOutputChannel: getOutputChannel,
       outputResult: outputResult,
+    };
+  };
+
+  const getTerminalMock = () => {
+    // mock getTerminal
+    const terminalSentTextList: string[] = [];
+    const getTerminal = () => {
+      return {
+        show: () => {
+          // do nothing
+        },
+        hide: () => {
+          // do nothing
+        },
+        dispose: () => {
+          // do nothing
+        },
+        name: 'test',
+        sendText: (text: string) => {
+          terminalSentTextList.push(text);
+        },
+        processId: (async () => 1)(),
+        creationOptions: {},
+        exitStatus: {
+          code: 0,
+          reason: 0,
+        },
+        state: {
+          isInteractedWith: false,
+        },
+      };
+    };
+    return {
+      getTerminal: getTerminal,
+      terminalSentTextList: terminalSentTextList,
     };
   };
 
@@ -77,5 +109,62 @@ suite('Command test', function () {
     assert.strictEqual(mock.outputResult.length, 2);
     assert.strictEqual(mock.outputResult[0], 'Command exited with code 1');
     assert.strictEqual(mock.outputResult[1], 'Commando done.');
+  });
+
+  test('Make sure runCommandInTerminal', async () => {
+    const mock = getTerminalMock();
+    sinon.stub(logger, 'getTerminal').callsFake(mock.getTerminal);
+    await command.runCommandInTerminal(commandObj, configObj);
+    assert.strictEqual(mock.terminalSentTextList.length, 2);
+    assert.strictEqual(mock.terminalSentTextList[0], 'clear');
+    assert.strictEqual(mock.terminalSentTextList[1], 'echo "test from test"');
+  });
+
+  test('Make sure getAutoFocus', () => {
+    // default true
+    assert.strictEqual(command.getAutoFocus(commandObj, configObj), true);
+    // command: default, config: false -> false
+    assert.strictEqual(command.getAutoFocus(commandObj, { ...configObj, autoFocus: false }), false);
+    // command: true, config: false -> true
+    assert.strictEqual(
+      command.getAutoFocus({ ...commandObj, autoFocus: true }, { ...configObj, autoFocus: false }),
+      true
+    );
+    // command: false, config: true -> false
+    assert.strictEqual(
+      command.getAutoFocus({ ...commandObj, autoFocus: false }, { ...configObj, autoFocus: true }),
+      false
+    );
+    // command: true, config: true -> true
+    assert.strictEqual(
+      command.getAutoFocus({ ...commandObj, autoFocus: true }, { ...configObj, autoFocus: true }),
+      true
+    );
+    // command: false, config: default -> false
+    assert.strictEqual(command.getAutoFocus({ ...commandObj, autoFocus: false }, configObj), false);
+  });
+
+  test('Make sure getAutoClear', () => {
+    // default true
+    assert.strictEqual(command.getAutoClear(commandObj, configObj), true);
+    // command: default, config: false -> false
+    assert.strictEqual(command.getAutoClear(commandObj, { ...configObj, autoClear: false }), false);
+    // command: true, config: false -> true
+    assert.strictEqual(
+      command.getAutoClear({ ...commandObj, autoClear: true }, { ...configObj, autoClear: false }),
+      true
+    );
+    // command: false, config: true -> false
+    assert.strictEqual(
+      command.getAutoClear({ ...commandObj, autoClear: false }, { ...configObj, autoClear: true }),
+      false
+    );
+    // command: true, config: true -> true
+    assert.strictEqual(
+      command.getAutoClear({ ...commandObj, autoClear: true }, { ...configObj, autoClear: true }),
+      true
+    );
+    // command: false, config: default -> false
+    assert.strictEqual(command.getAutoClear({ ...commandObj, autoClear: false }, configObj), false);
   });
 });
