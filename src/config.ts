24cd90be18f72dbfdcc91bb0d6061b4cf39bcdd2
SeeLog <seelog693@gmail.com>
2023-02-27 18:07:31 +0900

    [add] error class

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import * as vscode from 'vscode';
 import { IConfig, ICommand } from './interface';
 
+export class LoadConfigError extends Error {}
+
 export const getConfig = (): IConfig => {
   const config = vscode.workspace.getConfiguration('commando');
   const commandList = config.get<ICommand[]>('commands') ?? [];
@@ -8,10 +10,10 @@ export const getConfig = (): IConfig => {
 
   commandList.forEach((command) => {
     if (!command.name || !command.cmd) {
-      throw new Error('Invalid commando command. Must have name and cmd.');
+      throw new LoadConfigError('Invalid commando command. Must have name and cmd.');
     }
     if (alreadyUsedNameObj[command.name]) {
-      throw new Error(`Duplicate commando command name: ${command.name}`);
+      throw new LoadConfigError(`Duplicate commando command name: ${command.name}`);
     }
     alreadyUsedNameObj[command.name] = true;
   });
