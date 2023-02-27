0fa28075fa08515aefff49a7991f587a924b4110
SeeLog <seelog693@gmail.com>
2023-02-27 18:17:00 +0900

    [add] show error when config is invalid

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,23 @@
 import * as vscode from 'vscode';
 import { runCommand } from './command';
 import { ICommand, IConfig, ISpecialCommand } from './interface';
-import { getConfig } from './config';
+import { getConfig, LoadConfigError } from './config';
 
 export type PickerCommandItem = vscode.QuickPickItem & { commandoCommand: ICommand & ISpecialCommand };
 
 export const activate = (context: vscode.ExtensionContext) => {
-  let config: IConfig = getConfig();
+  let config: IConfig | undefined = undefined;
+  let configError: LoadConfigError | undefined = undefined;
+  try {
+    config = getConfig();
+  } catch (error) {
+    if (error instanceof LoadConfigError) {
+      vscode.window.showErrorMessage(error.message);
+      configError = error;
+    } else {
+      throw error;
+    }
+  }
 
   vscode.workspace.onDidChangeConfiguration(() => {
     console.log('Commando configuration changed.');
@@ -14,26 +25,30 @@ export const activate = (context: vscode.ExtensionContext) => {
   });
 
   const disposable = vscode.commands.registerCommand('commando.runCommand', async () => {
-    const pickItems = getPickerItems(config);
-    const selectedItem = await vscode.window.showQuickPick<PickerCommandItem>(pickItems, {
-      placeHolder: 'Select a command to run',
-    });
-    const selectedCommand = selectedItem?.commandoCommand;
-    if (!selectedCommand) {
-      return;
+    if (config !== undefined) {
+      const pickItems = getPickerItems(config);
+      const selectedItem = await vscode.window.showQuickPick<PickerCommandItem>(pickItems, {
+        placeHolder: 'Select a command to run',
+      });
+      const selectedCommand = selectedItem?.commandoCommand;
+      if (!selectedCommand) {
+        return;
+      }
+      if (selectedCommand.kind === 'openWorkspaceConfig') {
+        await vscode.commands.executeCommand('workbench.action.openWorkspaceSettingsFile');
+        return;
+      } else if (selectedCommand.kind === 'openUserConfig') {
+        // open json file
+        await vscode.commands.executeCommand('workbench.action.openSettingsJson');
+        return;
+      }
+      if (!selectedCommand.cmd) {
+        return;
+      }
+      runCommand(<ICommand>selectedCommand, config);
+    } else {
+      vscode.window.showErrorMessage(`Commando configuration is invalid.\n${configError?.message}`);
     }
-    if (selectedCommand.kind === 'openWorkspaceConfig') {
-      await vscode.commands.executeCommand('workbench.action.openWorkspaceSettingsFile');
-      return;
-    } else if (selectedCommand.kind === 'openUserConfig') {
-      // open json file
-      await vscode.commands.executeCommand('workbench.action.openSettingsJson');
-      return;
-    }
-    if (!selectedCommand.cmd) {
-      return;
-    }
-    runCommand(<ICommand>selectedCommand, config);
   });
 
   context.subscriptions.push(disposable);
