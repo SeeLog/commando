7c2b27a8756ccf37f05e5c3f3dee2bbed095463e
SeeLog <seelog693@gmail.com>
2023-03-05 15:38:14 +0900

    [fix] repeatedly channel creation

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,26 @@
 import * as vscode from 'vscode';
 
+export const outputChannelMap = new Map<string, vscode.OutputChannel>();
+
+/**
+ * Create or get output channel by name
+ * If output channel already exists, return it.
+ * Otherwise output channel does not exist, create it.
+ *
+ * @param name output channel name
+ * @returns output channel
+ */
 export const getOutputChannel = (name: string): vscode.OutputChannel => {
-  return vscode.window.createOutputChannel(name, 'log');
+  // Check if output channel already exists
+  const outputChannel = outputChannelMap.get(name);
+  if (outputChannel) {
+    return outputChannel;
+  }
+
+  // Create new output channel
+  const newOutputChannel = vscode.window.createOutputChannel(name);
+  outputChannelMap.set(name, newOutputChannel);
+  return newOutputChannel;
 };
 
 export const getTerminal = (name: string): vscode.Terminal => {
